61001b03a63a4e45e2bcd9fa70a898390540e467
Atharva0718 <115643856+Atharva0718@users.noreply.github.com>
2025-03-27 16:33:35 +0530

    Add files via upload

---
+++ 2223992.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+file_path = "D:/qualifier/Data Engineering/data - sample.xlsx"  
+df_attendance = pd.read_excel(file_path, sheet_name="Attendance_data")
+
+df_attendance["attendance_date"] = pd.to_datetime(df_attendance["attendance_date"], dayfirst=True)
+
+df_absent = df_attendance[df_attendance["status"] == "Absent"].sort_values(["student_id", "attendance_date"])
+
+def get_latest_absence_streak(df):
+    latest_listt = []
+    for student_id, group in df.groupby("student_id"):
+        sorted_dates = group["attendance_date"].tolist()
+        
+        listt = []
+        start_date = sorted_dates[0]
+        prev_date = start_date
+        count = 1
+
+        for i in range(1, len(sorted_dates)):
+            if (sorted_dates[i] - prev_date).days == 1:
+                count += 1
+            else:
+                if count > 3: 
+                    listt.append((start_date, prev_date, count))
+                start_date = sorted_dates[i]
+                count = 1
+            prev_date = sorted_dates[i]
+        
+        if count > 3:
+            listt.append((start_date, prev_date, count))
+        
+        if listt:
+            latest_streak = max(listt, key=lambda x: x[1])
+            latest_listt.append((student_id, latest_streak[0], latest_streak[1], latest_streak[2]))
+    
+    return pd.DataFrame(latest_listt, columns=["student_id", "absence_start_date", "absence_end_date", "total_absent_days"])
+
+df_absence_listt = get_latest_absence_streak(df_absent)
+
+output_file = "D:/qualifier/Data Engineering/absence_list.xlsx"
+df_absence_listt.to_excel(output_file, index=False)
+
+print(df_absence_listt)
+
